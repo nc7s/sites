3c3ece459a357531ffb890611354ac02847d0ae0
Blair Noctis <n@sail.ng>
2024-07-18 12:58:57 +0800

    ncts.me: rm obsolete {cibuild,makesig}.sh

--- ncts.me/cibuild.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e
-
-gem install asciidoctor
-hugo --gc --minify
-ln public/ji.html public/ji/index.html
-cp -r hugo.toml makesig.sh content static public/
--- ncts.me/makesig.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-shopt -s globstar
-
-sha256sum hugo.toml makesig.sh content/**/*.* static/**/*.* | gpg --clearsign | tee static/sums.asc.txt
-
-git add static/sums.asc.txt
