d7826751f43e358f85fd9a600032aa9bd09b1dbc
Blair Noctis <n@sail.ng>
2024-07-18 12:20:32 +0800

    ncts.me: build locally into own branch publish/ncts.me

---
+++ ncts.me/buildpub.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -euo pipefail
+shopt -s globstar
+
+FILES_SIGNED=$(ls hugo.toml buildpub.sh content/**/*.* static/**/*.*)
+COMMIT_ID=$(git rev-parse HEAD)
+
+../hugo.sh --gc --minify
+
+sha256sum $FILES_SIGNED | gpg --clearsign | tee public/sums.asc.txt
+cp -r hugo.toml buildpub.sh content static public/
+PUBLIC_FILES=$(cd public/ && ls)
+
+git switch --orphan publish/ncts.me
+
+(cd ../ && rm -rf $PUBLIC_FILES)
+(cd public/ && mv $PUBLIC_FILES ../../)
+(cd ../ &&
+	git add $PUBLIC_FILES &&
+	git commit -m "ncts.me: publish $COMMIT_ID" &&
+	rm -r $PUBLIC_FILES)
+
+git switch master
