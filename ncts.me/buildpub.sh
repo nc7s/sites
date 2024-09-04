f8a5925592096ea1b23cd90b27ad0f2adf54adaa
Blair Noctis <n@sail.ng>
2024-09-04 21:04:37 +0800

    ncts.me: skip drafts in buildpub.sh

--- ncts.me/buildpub.sh
+++ ncts.me/buildpub.sh
@@ -3,14 +3,29 @@
 set -euo pipefail
 shopt -s globstar
 
-FILES_SIGNED=$(ls hugo.toml buildpub.sh content/**/*.* static/**/*.*)
+DRAFTS=$(grep -E '^draft\s+=\s+true' content/**/*.* | awk -F: '{print $1}')
+CONTENTS_SIGNED=$(for i in content/**/*.*; do
+	case $DRAFTS in
+		*$i*)
+			;;
+		*)
+			echo $i
+			;;
+	esac
+done)
+
+FILES_SIGNED=$(ls hugo.toml buildpub.sh static/**/*.*)
 COMMIT_ID=$(git rev-parse HEAD)
 
 ../hugo.sh --gc --minify
 
-sha256sum $FILES_SIGNED | gpg --clearsign | tee public/sums.asc.txt
+(
+	date --iso-8601=seconds
+	echo sha256sum
+	sha256sum $CONTENTS_SIGNED $FILES_SIGNED
+) | gpg --clearsign | tee public/sums.asc.txt
 cp -r hugo.toml buildpub.sh content static public/
-PUBLIC_FILES=$(cd public/ && ls)
+PUBLIC_FILES=$(cd public/ && rm -f dummy $DRAFTS && ls)
 
 git switch publish/ncts.me
 
